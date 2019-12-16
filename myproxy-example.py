210ca6366669c23258db935642abdb13d82d7ccf
Gabriele <sucre.91@hotmail.it>
2019-12-16 13:53:42 +0100

    Minori fixes to myproxy wrapping

--- myproxy-example.py
+++ myproxy-example.py
@@ -10,21 +10,20 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='This is a process wrapper wich adds a myproxy retrieval satellite by default.')
     parser.add_argument("main", type=str, help="Provide the main process launch command as a string (surrounded by quotes).")
     parser.add_argument("--name", "-n", type=str, help="Provide a custom name for the pipeline process wrapper.")
-    parser.add_argument("--credname", "-k", type=str, help="Provide the credentials (-k) used to perform myproxy-init.", required=True)
-    parser.add_argument("--pass", "-p", type=str, help="Provide the myproxy password.", required=True)
+    parser.add_argument("--passw", "-p", type=str, help="Provide the myproxy password.", required=True)
     parser.add_argument("--voms", "-m", type=str, help="Provide the VOMS handle to add VOMS extensions to your certificate.")
 
     args = parser.parse_args()
 
     if args.name:
-        mainExe = KMA.subprocessWrapper(args.main, custonName = "pipeline")
+        mainExe = KMA.subprocessWrapper(args.main, customName = "pipeline")
     else:
-        mainExe = KMA.subprocessWrapper(args.main, custonName = "pipeline", customName = args.name)
+        mainExe = KMA.subprocessWrapper(args.main, customName = args.name)
 
     if args.voms:
-        sideExes = [KMA.makeWrapper("echo {} | myproxy-logon -k {} --voms {} -t 48 @ 172700".format(args.creds, args.voms), mainExe.is_alive, custonName = "myproxy-logon")]
+        sideExes = [KMA.makeWrapper("echo {} | myproxy-logon -d -S --voms {} -t 48 @ 172700".format(args.passw, args.voms), mainExe.is_alive, customName = "myproxy-logon")]
     else:
-        sideExes = [KMA.makeWrapper("myproxy-logon -k {} -t 48 @ 172700".format(args.creds), mainExe.is_alive, custonName = "myproxy-logon")]
+        sideExes = [KMA.makeWrapper("echo {} | myproxy-logon -d -S -t 48 @ 172700".format(args.passw), mainExe.is_alive, customName = "myproxy-logon")]
 
     KMA.main(mainExe, sideExes)
     
